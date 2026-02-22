f0f7078cadc02ec08176c9666fbae067491b148e
Tyler Matteson <tyler@agritheory.com>
2026-02-21 20:17:06 -0500

    chore: update pyproject and precommit

--- taxjar_erpnext/__init__.py
+++ taxjar_erpnext/__init__.py
@@ -1,1 +1,4 @@
+# Copyright (c) 2026, Washmore Development and contributors
+# For license information, please see license.txt
+
 __version__ = "1.0.0"
--- taxjar_erpnext/hooks.py
+++ taxjar_erpnext/hooks.py
@@ -1,4 +1,7 @@
-from . import __version__ as app_version
+# Copyright (c) 2026, Washmore Development and contributors
+# For license information, please see license.txt
+
+from . import __version__ as app_version  # noqa: F401
 
 app_name = "taxjar_erpnext"
 app_title = "TaxJar ERPNext"
@@ -6,6 +9,7 @@ app_publisher = "Washmore Development"
 app_description = "TaxJar Integration with ERPNext - Multi-Company Support"
 app_email = "dev@washmore.app"
 app_license = "MIT"
+required_apps = ["erpnext"]
 
 # Document Events
 # ---------------
@@ -14,7 +18,7 @@ app_license = "MIT"
 doc_events = {
 	"Sales Invoice": {
 		"on_submit": "taxjar_erpnext.taxjar_erpnext.taxjar_erpnext.create_transaction",
-		"on_cancel": "taxjar_erpnext.taxjar_erpnext.taxjar_erpnext.delete_transaction"
+		"on_cancel": "taxjar_erpnext.taxjar_erpnext.taxjar_erpnext.delete_transaction",
 	},
 	"Sales Order": {
 		"on_submit": "taxjar_erpnext.taxjar_erpnext.taxjar_erpnext.notify_non_nexus_sales_order"
